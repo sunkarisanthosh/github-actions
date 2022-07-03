3627bc7266e1a74be7cb0b674ec160a3ad4aff20
sunkarisanthosh <77742585+sunkarisanthosh@users.noreply.github.com>
2022-07-03 19:56:24 +0100

    Update tests1.py

--- dags/tests1.py
+++ dags/tests1.py
@@ -1,3 +1,17 @@
 inport airflow
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from datetime import timedelta
+default_args = {
+    'start_date': airflow.utils.dates.days_ago(0),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG{
+    'test1',
+    default_args=default_args,
+    description='automated deployments test3'
+    schedule_interval=NONE,
+    dagrun_timeout=timedelta(minutes=20))
+  
