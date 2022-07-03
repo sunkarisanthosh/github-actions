8d223a0982b7b2a898e5690f9375274db384ebf9
sunkarisanthosh <77742585+sunkarisanthosh@users.noreply.github.com>
2022-07-03 20:37:10 +0100

    Update testdag.py

--- dags/testdag.py
+++ dags/testdag.py
@@ -1,17 +1,21 @@
-import airflow
+from datetime import datetime
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
-from datetime import timedelta
-default_args = {
-    'start_date': airflow.utils.dates.days_ago(0),
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5)
-}
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators import MultiplyBy5Operator
 
-dag = DAG{
-    'test1',
-    default_args=default_args,
-    description='hjh'
-    schedule_interval=NONE,
-    dagrun_timeout=timedelta(minutes=20))
-  
+def print_hello():
+ return 'Hello Wolrd'
+
+dag = DAG('hello_world', description='Hello world example', schedule_interval='0 12 * * *', start_date=datetime(2017, 3, 20), catchup=False)
+
+dummy_operator = DummyOperator(task_id='dummy_task', retries = 3, dag=dag)
+
+hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+
+multiplyby5_operator = MultiplyBy5Operator(my_operator_param='my_operator_param',
+                                task_id='multiplyby5_task', dag=dag)
+
+dummy_operator >> hello_operator
+
+dummy_operator >> multiplyby5_operator
