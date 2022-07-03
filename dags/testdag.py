f20ae0c60f1d9e6e3bee2529bb29b508f0569508
sunkarisanthosh <77742585+sunkarisanthosh@users.noreply.github.com>
2022-07-03 17:22:52 +0100

    Update testdag.py

--- dags/testdag.py
+++ dags/testdag.py
@@ -3,7 +3,6 @@
 """Example Airflow DAG that creates a Cloud Dataproc cluster, runs the Hadoop
 wordcount example, and deletes the cluster.
 
-This DAG relies on three Airflow variables
 https://airflow.apache.org/docs/apache-airflow/stable/concepts/variables.html
 * gcp_project - Google Cloud Project to use for the Cloud Dataproc cluster.
 * gce_region - Google Compute Engine region where Cloud Dataproc cluster should be
