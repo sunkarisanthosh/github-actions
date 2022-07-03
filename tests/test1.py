f24700615b1885884ebc2d7964da76cb9c3f0730
sunkarisanthosh <77742585+sunkarisanthosh@users.noreply.github.com>
2022-07-03 20:05:11 +0100

    Create test1.py

---
+++ tests/test1.py
@@ -0,0 +1,26 @@
+
+
+import os
+import sys
+
+import pytest
+from airflow.models import DagBag
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../dags"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "../dags/utilities"))
+
+
+
+@pytest.fixture(params=["../dags/"])
+def dag_bag(request):
+    return DagBag(dag_folder=request.param, include_examples=False)
+
+
+
+
+
+def test_desc_len_greater_than_fifteen(dag_bag):
+    for dag, dag in dag_bag.dags.items():
+        assert len(dag.description) > 10
+
+
