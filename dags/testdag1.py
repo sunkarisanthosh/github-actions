d39216b970344cf8cbe2356d1e77c5e33bbd7be1
sunkarisanthosh <77742585+sunkarisanthosh@users.noreply.github.com>
2022-07-04 10:29:56 +0100

    Update testdag1.py

--- dags/testdag1.py
+++ dags/testdag1.py
@@ -1,10 +1,19 @@
-import pendulum
+import os
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.models.baseoperator import chain
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.providers.amazon.aws.operators.glue_crawler import AwsGlueCrawlerOperator
+from airflow.utils.dates import days_ago
+
+DAG_ID = os.path.basename(__file__).replace(".py", "")
 
 with DAG(
     dag_id='my_dag',
-    description = "This is the sample dag for testing"
-    start_date=pendulum.datetime(2016, 1, 1, tz="UTC"),
-    schedule_interval='@daily',
+    description = "this is a sample dag for unit testing"
+    schedule_interval='None',
     catchup=False,
     default_args={'retries': 2},
 ) as dag:
