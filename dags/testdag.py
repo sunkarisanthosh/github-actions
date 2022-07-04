d2d99bf6256b1e55644646a9517548cd1a0eb02b
sunkarisanthosh <77742585+sunkarisanthosh@users.noreply.github.com>
2022-07-04 12:23:07 +0100

    Update testdag.py

--- dags/testdag.py
+++ dags/testdag.py
@@ -2,15 +2,15 @@ import os
 from datetime import timedelta
 
 from airflow import DAG
-from airflow.models import Variable
 from airflow.models.baseoperator import chain
 from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
+from airflow.providers.amazon.aws.operators.glue import AwsGlueJobOperator
 from airflow.utils.dates import days_ago
 
 DAG_ID = os.path.basename(__file__).replace(".py", "")
 
-S3_BUCKET = Variable.get("db_volt_bucket")
+TABLES = ["users", "venue", "category", "date", "event", "listing", "sales"]
 
 DEFAULT_ARGS = {
     "owner": "db-volt",
@@ -22,44 +22,33 @@ DEFAULT_ARGS = {
 
 with DAG(
     dag_id=DAG_ID,
-    description="writing a sample dag for unit testing",
+    description="Run AWS Glue ETL Jobs - raw data to refined (silver) data",
     default_args=DEFAULT_ARGS,
-    dagrun_timeout=timedelta(minutes=5),
+    dagrun_timeout=timedelta(minutes=15),
     start_date=days_ago(1),
     schedule_interval=None,
-    tags=["dag testing"],
+    tags=["data lake demo", "refined", "silver"],
 ) as dag:
     begin = DummyOperator(task_id="begin")
 
     end = DummyOperator(task_id="end")
 
-#     
-
-    delete_demo_s3_objects = BashOperator(
-        task_id="delete_demo_gcs_bucket_objects",
-        bash_command=f'aws s3 rm "s3://{S3_BUCKET}/tickit/" --recursive',
+    list_glue_tables = BashOperator(
+        task_id="list_glue_tables",
+        bash_command="""aws glue get-tables --database-name tickit_demo \
+                          --query 'TableList[].Name' --expression "refined_*"  \
+                          --output table""",
     )
 
-    list_demo_s3_objects = BashOperator(
-        task_id="list_demo_s3_objects",
-        bash_command=f"aws s3api list-objects-v2 --bucket {S3_BUCKET} --prefix tickit/",
-    )
+    for table in TABLES:
+        start_jobs_refined = AwsGlueJobOperator(
+            task_id=f"start_job_{table}_refined",
+            job_name=f"tickit_public_{table}_refine",
+        )
 
-    delete_demo_catalog = BashOperator(
-        task_id="delete_demo_catalog",
-        bash_command='aws glue delete-database --name tickit_demo || echo "Database tickit_demo not found."',
-    )
-
-    create_demo_catalog = BashOperator(
-        task_id="create_demo_catalog",
-        bash_command="""aws glue create-database --database-input \
-            '{"Name": "tickit_demo", "Description": "Datasets from AWS E-commerce TICKIT relational database"}'""",
-    )
-
-chain(
-    begin,
-    (delete_demo_s3_objects, delete_demo_catalog),
-    (list_demo_s3_objects, create_demo_catalog),
-    end,
-)
-# 
+        chain(
+            begin,
+            start_jobs_refined,
+            list_glue_tables,
+            end,
+        )
