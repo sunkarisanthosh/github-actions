ffe9df13cebe5b6f254db8bffffe85790e32a10c
sunkarisanthosh <77742585+sunkarisanthosh@users.noreply.github.com>
2022-07-04 10:45:41 +0100

    Update tests1.py

--- tests/tests1.py
+++ tests/tests1.py
@@ -1,9 +1,10 @@
+
 import os
 import sys
 
 import pytest
 from airflow.models import DagBag
 
-def test_desc_len_greater_than_fifteen(dag_bag):
-    for dag_id, dag in dag_bag.dags.items():
+def test_desc_len_greater_than_fifteen(dag_id):
+    
         assert len(dag.description) > 15
